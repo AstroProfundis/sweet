2ddb2e0ae4292602d73de81e736f3c0dd8fbfe98
NetPuter <netputer@gmail.com>
2012-09-03 23:43:06 +0800

    Fetch data from Twitter

--- fetch.php
+++ fetch.php
@@ -1,14 +1,56 @@
 <?php
-	require('config.php');
-	require('libs/twitter.php');
-	require('common.php');
 
+	require_once('config.php');
+	require_once('libs/twitter.php');
+	require_once('common.php');
+
+	// /*
 	$data = json_decode(file_get_contents('data_1.json'), TRUE);
+
+	foreach ($data as $tweet) {
+		$content[$tweet['id_str']] = construct($tweet);
+	}
+
+	$data = json_decode(file_get_contents('data_2.json'), TRUE);
+
+	foreach ($data as $tweet) {
+		$content[$tweet['id_str']] = construct($tweet, TRUE);
+	}
+
+	krsort($content);
+
+	echo json_encode(array_values($content));
+	exit;
+	// */
+
+	$connection = new TwitterOAuth($config['consumer_key'], $config['consumer_secret'], $config['access_token'], $config['access_token_secret']);
+
+	$timeline_a = $connection->get('statuses/user_timeline', array(
+		'screen_name' => 'NetPuter',
+		'count' => $config['count'],
+		'since_id' => $config['since_id'],
+		'include_rts' => 1,
+		'include_entities' => 1,
+	));
+
+	$timeline_b = $connection->get('statuses/user_timeline', array(
+		'screen_name' => 'Regulusw',
+		'count' => $config['count'],
+		'since_id' => $config['since_id'],
+		'include_rts' => 1,
+		'include_entities' => 1,
+	));
 
 	$content = array();
 	
-	foreach ($data as $d) {
-		$content[] = construct($d);
+	foreach ($timeline_a as $tweet) {
+		$content[$tweet['id_str']] = construct($tweet);
 	}
 
-	echo json_encode($content);
+	foreach ($timeline_b as $tweet) {
+		$content[$tweet['id_str']] = construct($tweet);
+	}
+
+	krsort($content);
+
+	echo json_encode(array_values($content));
