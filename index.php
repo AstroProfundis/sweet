672c8e04ca07953dd08f30b6efafee1d234fd6b3
NetPuter <netputer@gmail.com>
2012-09-09 18:37:20 +0800

    Add `apple-mobile-web-app-capable`

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>Sweet</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
 	<link rel="shortcut icon" href="img/favicon.ico" />
 	<link rel="apple-touch-icon" href="img/touch-icon.png" />
